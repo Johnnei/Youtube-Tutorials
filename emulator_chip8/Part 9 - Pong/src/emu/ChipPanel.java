2ac42151fa9e6d84cba443f786774b7b9f92407b
Johnnei <johnnei07@live.nl>
2012-12-21 19:01:25 +0100

    Part 9 Source

---
+++ emulator_chip8/Part 9 - Pong/src/emu/ChipPanel.java
@@ -0,0 +1,34 @@
+package emu;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+import javax.swing.JPanel;
+
+import chip.Chip;
+
+public class ChipPanel extends JPanel {
+	
+	private static final long serialVersionUID = 1L;
+	private Chip chip;
+	
+	public ChipPanel(Chip chip) {
+		this.chip = chip;
+	}
+	
+	public void paint(Graphics g) {
+		byte[] display = chip.getDisplay();
+		for(int i = 0; i < display.length; i++) {
+			if(display[i] == 0)
+				g.setColor(Color.BLACK);
+			else
+				g.setColor(Color.WHITE);
+			
+			int x = (i % 64);
+			int y = (int)Math.floor(i / 64);
+			
+			g.fillRect(x * 10, y * 10, 10, 10);
+		}
+	}
+
+}
