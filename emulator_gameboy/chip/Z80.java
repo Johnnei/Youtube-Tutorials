63dbf20b2f257b8b5521d221d580592d84d56950
Johnnei <johnnei07@live.nl>
2013-01-25 19:33:38 +0100

    Implemented opcode: C5, D5, E5, F5, C1, D1, E1, F1, CD and C9

--- emulator_gameboy/chip/Z80.java
+++ emulator_gameboy/chip/Z80.java
@@ -16,6 +16,24 @@ public class Z80 {
 	public static final int REGISTER_H = 5;
 	public static final int REGISTER_L = 6;
 	public static final int REGISTER_F = 7;
+
+	/**
+	 * The bit in {@link #REGISTER_F} that represents if the last math operation result was: 0
+	 */
+	public static final int FLAG_ZERO = 7;
+	/**
+	 * The bit in {@link #REGISTER_F} that represents if the last math operation was a subtraction
+	 */
+	public static final int FLAG_SUBTRACT = 6;
+	/**
+	 * The bit in {@link #REGISTER_F} that represents if the last math operation caused a carry on the lower nibble (result >= 16)
+	 */
+	public static final int FLAG_HALF_CARRY = 5;
+	/**
+	 * The bit in {@link #REGISTER_F} that represents if the last math operation caused a carry (result > 255)<Br/>
+	 * This bit is also set of {@link #REGISTER_A} is the smaller value during the CP Instruction
+	 */
+	public static final int FLAG_CARRY = 4;
 	// END FINAL
 
 	/**
@@ -49,7 +67,7 @@ public class Z80 {
 	 * Bit 5: Half Carry Flag (H)<br/>
 	 * Bit 4: Carry Flag (C)<br/>
 	 */
-	private char[] registers;
+	private char[] register;
 	/**
 	 * The StackPointer
 	 */
@@ -67,22 +85,147 @@ public class Z80 {
 	public void reset() {
 		display = new byte[23040];
 		memory = new char[65535];
-		registers = new char[8];
+		register = new char[8];
 		pc = 0x0;
 		sp = 0xFFFE;
 	}
 
 	/**
-	 * Processes a single opcode and returns the amount of "cycles" it took to
-	 * process it
+	 * Processes a single opcode and returns the amount of "cycles" it took to process it
 	 * 
 	 * @return The amount of cycles this operation took
 	 */
 	public int run() {
 		// TODO Fetch opcode
-		// TODO Parse opcode
-		return 4; // A "NOP" Instruction takes 4 cycles, a NOP does nothing so
-					// we will default it to 4 cycles
+		int opcode = 0x0;
+		switch (opcode) {
+		// PUSH n
+			case 0xF5: { // PUSH AF
+				push((byte) register[REGISTER_A]);
+				push((byte) register[REGISTER_F]);
+				pc += 1;
+				return 16;
+			}
+
+			case 0xC5: { // PUSH BC
+				push((byte) register[REGISTER_B]);
+				push((byte) register[REGISTER_C]);
+				pc += 1;
+				return 16;
+			}
+
+			case 0xD5: { // PUSH DE
+				push((byte) register[REGISTER_D]);
+				push((byte) register[REGISTER_E]);
+				pc += 1;
+				return 16;
+			}
+
+			case 0xE5: { // PUSH HL
+				push((byte) register[REGISTER_H]);
+				push((byte) register[REGISTER_L]);
+				pc += 1;
+				return 16;
+			}
+
+			// POP nn
+			case 0xF1: { // POP AF
+				register[REGISTER_F] = (char) pop();
+				register[REGISTER_A] = (char) pop();
+				pc += 1;
+				return 12;
+			}
+
+			case 0xC1: { // POP BC
+				register[REGISTER_C] = (char) pop();
+				register[REGISTER_B] = (char) pop();
+				pc += 1;
+				return 12;
+			}
+
+			case 0xD1: { // POP DE
+				register[REGISTER_E] = (char) pop();
+				register[REGISTER_D] = (char) pop();
+				pc += 1;
+				return 12;
+			}
+
+			case 0xE1: { // POP HL
+				register[REGISTER_L] = (char) pop();
+				register[REGISTER_H] = (char) pop();
+				pc += 1;
+				return 12;
+			}
+
+			// CALL
+			case 0xCD: { // CALL nn
+				int nn = memory[pc + 1] | (memory[pc + 2] << 8);
+				call(nn);
+				return 12;
+			}
+
+			// RETURN
+			case 0xC9: { // RET
+				int nn = pop() | (pop() << 8);
+				jump(nn);
+				return 8;
+			}
+
+			default:
+				return 4;
+		}
+	}
+
+	/**
+	 * Merges two registers into the 16-bit value
+	 * 
+	 * @param leftRegister The index of the left byte (upper 8 bits) register (A, B, D or H)
+	 * @param rightRegister The index of the right byte (lower 8 bits) register
+	 * @return The 16 bit register value
+	 */
+	private int registerPair(int leftRegister, int rightRegister) {
+		return (register[leftRegister] << 8) | register[rightRegister];
+	}
+
+	/**
+	 * Call a specific address<br/>
+	 * Stores the next operation on the stack
+	 * 
+	 * @param nn The address to jump to
+	 */
+	private void call(int nn) {
+		int newAddress = pc + 1;
+		push((byte) (newAddress >> 8));
+		push((byte) (newAddress));
+		jump(nn);
+	}
+
+	/**
+	 * Jumps to the given address
+	 * 
+	 * @param nn The address to jump to
+	 */
+	private void jump(int nn) {
+		pc = (char) nn;
+	}
+
+	/**
+	 * Pops a byte from the stack
+	 * 
+	 * @return The next byte on the stack
+	 */
+	private int pop() {
+		return memory[sp++];
+	}
+
+	/**
+	 * Pushes a byte onto the stack
+	 * 
+	 * @param n The byte to be pushed
+	 */
+	private void push(byte n) {
+		memory[sp] = (char) n;
+		--sp;
 	}
 
 	/**
@@ -131,8 +274,7 @@ public class Z80 {
 	/**
 	 * Loads the cartridge into the memory
 	 * 
-	 * @param file
-	 *            The location of the cartrigde
+	 * @param file The location of the cartrigde
 	 */
 	public void loadCartridge(String file) {
 		DataInputStream input = null;
